b8a20a05a7ee5975ee178c4e661e3886a113b931
irvinakocan <44883640+irvinakocan@users.noreply.github.com>
2023-12-09 18:14:25 +0100

    UI setup
    Implemented user interface, network calls and handled possible errors

--- GitHub API networking/ViewController.swift
+++ GitHub API networking/ViewController.swift
@@ -8,12 +8,42 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var gitHubUser: GitHubUser!
+    var gitHubUserView: GitHubUserView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        Task {
+            do {
+                gitHubUser = try await Request.getUser()
+                gitHubUserView = GitHubUserView(model: gitHubUser)
+                setGitHubUserView()
+            }
+            catch MyError.INVALID_DECODING {
+                print("Invalid decoding problem.")
+            }
+            catch MyError.INVALID_RESPONSE {
+                print("Invalid response.")
+            }
+            catch MyError.BAD_URL {
+                print("The URL was bad.")
+            }
+            catch {
+                print(error.localizedDescription)
+            }
+        }
     }
-
-
+    
+    private func setGitHubUserView() {
+        view.addSubview(gitHubUserView)
+        gitHubUserView.translatesAutoresizingMaskIntoConstraints = false
+        gitHubUserView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20).isActive = true
+        gitHubUserView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 50).isActive = true
+        gitHubUserView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20).isActive = true
+        gitHubUserView.heightAnchor.constraint(equalTo: view.heightAnchor, multiplier: 1/3).isActive = true
+    }
 }
 

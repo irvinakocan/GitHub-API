eed455f56b62df0d21450acabdfc91ba1129043b
irvinakocan <44883640+irvinakocan@users.noreply.github.com>
2023-12-09 18:12:31 +0100

    File with adequate constants for network calls

---
+++ GitHub API networking/Constants.swift
@@ -0,0 +1,12 @@
+//
+//  Constants.swift
+//  GitHub API networking
+//
+//  Created by Macbook Air 2017 on 9. 12. 2023..
+//
+
+import Foundation
+
+let GITHUB_URL = "https://api.github.com"
+let GET_USER_ENDPOINT = "/users/"
+let USERNAMES = ["seallen0400", "irvinakocan", "bhadzic2"]
